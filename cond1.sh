92b7887638f4a5840eec94706ea1c75fdf564c88
Dmitrii Voroshilov <Dmitrii_Voroshilov@epam.com>
2022-07-09 21:09:34 +0300

    rev:0.1

--- cond1.sh
+++ cond1.sh
@@ -3,3 +3,15 @@
 IFS=',' read -r -a array <<< "$1"
 
 #Write your code here
+
+sum=0
+
+for i in "${array[@]}"
+do
+    if [ $((i%2)) -eq 0 ]
+    then
+        sum=$((sum+i))
+    fi
+done
+
+echo $sum
