41aa00825d54a1ef73ff11f3c5ed6634bf9a5938
Dmitrii Voroshilov <Dmitrii_Voroshilov@epam.com>
2022-07-10 15:08:42 +0300

    check_string script rev:0.1

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 # Place your code here
+
+str=$1
+num=0
+sym=0
+let=0
+
+for i in $(echo "$str" | grep -o .)
+do
+case "$i" in
+[0-9] )
+	((num++));;
+[\!\@\#\$\%\^\&\(\)\_\+\*] ) 
+	((sym++));;
+[a-z-A-Z] )
+	((let++));;
+esac
+done
+echo "Numbers: $num Symbols: $sym Letters: $let"
